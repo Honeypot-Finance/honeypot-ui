0f95d33be33af65c5021edf55e88950ffe234bee
Hongming Wang <70198795+rabbitblood@users.noreply.github.com>
2025-02-04 18:37:40 -0800

    Delete dist directory

--- dist/testButton.d.ts
+++
@@ -1,7 +0,0 @@
-import { ButtonProps } from "@nextui-org/react";
-import React from "react";
-interface CustomButtonProps extends ButtonProps {
-    styleMode?: "plain" | "primary";
-}
-export declare const Button: React.ForwardRefExoticComponent<Omit<CustomButtonProps, "ref"> & React.RefAttributes<HTMLButtonElement>>;
-export default Button;
